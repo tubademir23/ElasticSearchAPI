42419b86f34b90135bb69392dfed497c56376f43
t d <tubademir23@gmail.com>
2020-04-28 15:38:55 +0300

    elastic search api

---
+++ es-java-ent/src/main/java/App.java
@@ -0,0 +1,116 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
+import org.elasticsearch.index.reindex.DeleteByQueryRequestBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+public class App {
+	@SuppressWarnings("deprecation")
+	public static TransportClient client;
+	public static final String CLUSTER_NAME = "elasticsearch";
+	public static final String HOST_NAME = "localhost";
+	public static final String INDEX_NAME = "product";
+	public static final String TYPE_NAME = "_doc";
+
+	public App() {
+		client = createClient();
+	}
+
+	public TransportClient createClient() {
+		Settings settings = Settings.builder().put("cluster.name", CLUSTER_NAME).build();
+		// TransportClient client = null;
+		try {
+			client = new PreBuiltTransportClient(settings)
+					.addTransportAddress(new TransportAddress(InetAddress.getByName(HOST_NAME), 9300));
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		List<DiscoveryNode> nodes = client.listedNodes();
+		for (DiscoveryNode discoveryNode : nodes) {
+			System.out.println(discoveryNode);
+		}
+		return client;
+	}
+
+	public static void main(String args[]) {
+		// on startup
+		App app = new App();
+		// app.index();
+		// app.get();
+		// app.search();
+		// app.delete();
+		// app.deleteByQuery("brand", "asus1");
+		client.close();
+	}
+
+	public void toString(Map<String, Object> json) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Brand: " + json.get("brand"));
+		sb.append("\tcolor: " + json.get("color"));
+		sb.append("\tprovider: " + json.get("provider"));
+		sb.append("\tprice: " + json.get("price"));
+		System.out.println(sb.toString());
+	}
+
+	public void deleteByQuery(String filter, String filterValue) {
+		BulkByScrollResponse response = new DeleteByQueryRequestBuilder(client, DeleteByQueryAction.INSTANCE)
+				.filter(QueryBuilders.matchQuery(filter, filterValue)).source(INDEX_NAME).get();
+		long deleted = response.getDeleted();
+	}
+
+	public void delete() {
+		DeleteResponse response = client.prepareDelete(INDEX_NAME, TYPE_NAME, "1").get();
+		System.out.println(response);
+	}
+
+	public void search() {
+		SearchResponse response = client.prepareSearch(INDEX_NAME).setTypes(TYPE_NAME)
+				.setQuery(QueryBuilders.matchQuery("brand", "asus1")).get();
+		System.out.println(response);
+		SearchHit[] hits = response.getHits().getHits();
+		for (SearchHit sh : hits) {
+			// System.out.println(sh);
+			Map<String, Object> json = sh.getSourceAsMap();
+			toString(json);
+		}
+	}
+
+	public IndexResponse index() {
+		Map<String, Object> json = new HashMap<String, Object>();
+		json.put("brand", "asusxxxx ");
+		json.put("color", "grey");
+		json.put("provider", "Asus -TR");
+		json.put("price", "4500");
+		IndexResponse response = client.prepareIndex(INDEX_NAME, TYPE_NAME, "1").setSource(json, XContentType.JSON)
+				.get();
+		System.out.println(response.getId());
+		return response;
+	}
+
+	public void get() {
+		GetResponse response = client.prepareGet(INDEX_NAME, TYPE_NAME, "1").get();
+		Map<String, Object> json = response.getSource();
+		toString(json);
+
+	}
+
+}
