f6f49d6372327175873de1d74a51fc5abfa4586a
Tuba Demir <tubademir23@gmail.com>
2020-06-05 17:00:53 +0300

    .

--- es-java-ent/src/main/java/App.java
+++ es-java-ent/src/main/java/App.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
@@ -20,7 +21,9 @@ import org.elasticsearch.index.reindex.DeleteByQueryAction;
 import org.elasticsearch.index.reindex.DeleteByQueryRequestBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+@SuppressWarnings("deprecation")
 public class App {
 	@SuppressWarnings("deprecation")
 	public static TransportClient client;
@@ -28,21 +31,21 @@ public class App {
 	public static final String HOST_NAME = "localhost";
 	public static final String INDEX_NAME = "product";
 	public static final String TYPE_NAME = "_doc";
-
-	public static final int _ID = 1;
+	private static final Logger LOG = LogManager.getLogger(LogApp.class);
+	public static String _ID = "1";
 	public App() {
 		client = createClient();
 	}
 
 	public TransportClient createClient() {
-		Settings settings = Settings.builder().put("cluster.name", CLUSTER_NAME)
+		Settings settings = Settings.builder()//.put("cluster.name", CLUSTER_NAME)
 				.build();
 		// TransportClient client = null;
 		try {
 			client = new PreBuiltTransportClient(settings).addTransportAddress(
 					new TransportAddress(InetAddress.getByName(HOST_NAME),
 							9300));
-
+			LOG.info("\nClient successfully created\nNODES:");
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,13 +53,15 @@ public class App {
 
 		try {
 			List<DiscoveryNode> nodes = client.listedNodes();
+			int i=0;
 			for (DiscoveryNode discoveryNode : nodes) {
-				System.out.println(discoveryNode);
+				System.out.printf("%d : %s\n",++i,discoveryNode);
 			}
 		} catch (NoNodeAvailableException e) {
 			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
+			LOG.error(e.getMessage());
 		} catch (Exception ex) {
+			LOG.error(ex.getMessage());
 			ex.printStackTrace();
 		}
 
@@ -68,16 +73,16 @@ public class App {
 		// on startup
 		App app = new App();
 		app.index();
-		app.get();
-		/*
-		 * try { app.sendFile(); } catch (Exception e) { // TODO Auto-generated
-		 * catch block e.printStackTrace(); }
-		 */
-
-		// app.get();
-		// app.search();
+		//app.get();
+		//app.search();
 		// app.delete();
 		// app.deleteByQuery("brand", "asus1");
+		 app.deleteByQueryRequest("brand", "asus1");
+		 /*
+			 * try { app.sendFile(); } catch (Exception e) { // TODO Auto-generated
+			 * catch block e.printStackTrace(); }
+			 */
+
 		client.close();
 	}
 
@@ -116,19 +121,37 @@ public class App {
 						.filter(QueryBuilders.matchQuery(filter, filterValue))
 						.source(INDEX_NAME).get();
 		long deleted = response.getDeleted();
+		LOG.info("Deleted item by query: {0}", deleted);
 	}
-
+	//execute it as asynchronously, call execute instead of get..
+	public void deleteByQueryRequest(String filter, String filterValue) {
+		new DeleteByQueryRequestBuilder(client,
+			DeleteByQueryAction.INSTANCE)
+				.filter(QueryBuilders.matchQuery(filter, filterValue))
+				.source(INDEX_NAME)
+				.execute(new ActionListener<BulkByScrollResponse>() {   
+			        public void onResponse(BulkByScrollResponse response) {
+			            long deleted = response.getDeleted(); 
+			            //System.out.println("\nSuccessfully delete : "+deleted);
+			            LOG.info("\nSuccessfully delete : {0}", deleted);
+			        }
+			        public void onFailure(Exception e) {
+			            //System.out.println("\nDelete failure: "+e.getMessage());
+			        	LOG.error("\nDeleted failure : "+e.getMessage());
+			        }
+				});
+	}
 	public void delete() {
 		DeleteResponse response = client
 				.prepareDelete(INDEX_NAME, TYPE_NAME, _ID).get();
-		System.out.println(response);
+		LOG.info("\nDelete Result: %s", response.getId());
 	}
 
 	public void search() {
 		SearchResponse response = client.prepareSearch(INDEX_NAME)
 				.setTypes(TYPE_NAME)
 				.setQuery(QueryBuilders.matchQuery("brand", "asus1")).get();
-		System.out.println(response);
+		LOG.info("\nSearch Result: %s", response.toString());
 		SearchHit[] hits = response.getHits().getHits();
 		for (SearchHit sh : hits) {
 			// System.out.println(sh);
@@ -139,14 +162,22 @@ public class App {
 	// index noun means -> document
 	// index verb means -> inserting
 	public IndexResponse index() {
+		/*
+		 * String json = "{" +
+        "\"user\":\"kimchy\"," +
+        "\"postDate\":\"2013-01-30\"," +
+        "\"message\":\"trying out Elasticsearch\"" +
+    		"}";
+		 * */
 		Map<String, Object> json = new HashMap<String, Object>();
 		json.put("brand", "asusxxxx ");
 		json.put("color", "grey");
 		json.put("provider", "Asus -TR");
 		json.put("price", "4500");
-		IndexResponse response = client.prepareIndex(INDEX_NAME, TYPE_NAME, _ID)
+		IndexResponse response = client.prepareIndex(INDEX_NAME, TYPE_NAME)
 				.setSource(json, XContentType.JSON).get();
-		System.out.println(response.getId());
+		LOG.info(String.format("\nResponse Result:%s ID: %s\n" ,response.getResult().name() ,response.getId()));
+		_ID=response.getId();
 		return response;
 	}
 
